f0576b0621d40864bd5af3dc6e05b5721467a395
mansiHaldankar <mansihaldankar15@gmail.com>
2019-12-11 08:39:37 +0530

    First commit to fitness tracker

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC1ABSqRszOUqcs1wE90U4RAHDMEr6_ioQ",
+    authDomain: "ng-fitness-tracker-e267f.firebaseapp.com",
+    databaseURL: "https://ng-fitness-tracker-e267f.firebaseio.com",
+    projectId: "ng-fitness-tracker-e267f",
+    storageBucket: "ng-fitness-tracker-e267f.appspot.com",
+    messagingSenderId: "610922281731"
+  }
+};
